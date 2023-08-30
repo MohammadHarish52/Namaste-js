a43d775518afcfa0152510efc7542069c58a0343
tachyonok <tachyonparticle123@gmail.com>
2023-08-30 12:27:49 +0530

    first commit

---
+++ hoisiting/app.js
@@ -0,0 +1,20 @@
+//hoisiting
+
+getName();
+//returns the function since it is in general form and the memory block will store it like a whole function itself
+newName();
+//returns undefined since arrow functions behave diffrently in js
+console.log(x);
+//returns undefined
+
+
+
+let x = 7;
+
+function getName(){
+    console.log("my name is khan");
+}
+
+const newName =()=>{
+    console.log("My name is harish")
+}

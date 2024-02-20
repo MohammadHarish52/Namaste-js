188220c4a1889a3875f7f706cb32aa493e554837
tachyonok <tachyonparticle123@gmail.com>
2024-02-20 12:30:36 +0530

    Added comments to the notes

--- function and variable environement/app.js
+++ function and variable environement/app.js
@@ -1,17 +1,21 @@
 let x = 1;
-a();// 9 will be printed from local memory space and after that the execution context for the a function will be removed from the stack
-b();// 99 will be printed from local memory space and after that the execution context for the b function will be removed from the stack
-console.log(x); // since the value of x  = 1 in local memory space and the earlier two exection context have already been deleted from the memory space 
+a(); // 9 will be printed from local memory space
+// and after that the execution context for the
+// a function will be removed from the stack
+b(); // 99 will be printed from local memory space
+// and after that the execution context for the
+// b function will be removed from the stack
+console.log(x); // since the value of x = 1 in local memory
+// space and the earlier two exection context
+//have already been deleted from the memory space
 // the answer will be 1 taken from the local memory space
 
-
-
-function a(){
-    let x = 9;
-    console.log(x);
+function a() {
+  let x = 9;
+  console.log(x);
 }
 
-function b(){
-    let x = 99;
-    console.log(x);
+function b() {
+  let x = 99;
+  console.log(x);
 }
--- lexical/app.js
+++ lexical/app.js
@@ -1,22 +1,28 @@
-function a(){
-c();
-  function c(){
-    console.log(b); 
- }
+function a() {
+  c();
+  function c() {
+    console.log(b);
+  }
 }
-var b=10;
+var b = 10;
 a();
 
 // Lexical environment = EC's Local Memory + Reference to Lexical Environment of its parent.
 
-// 1) Lexical Environment of its parent is the scope where a function is physically present or defined. So, suppose a function x(), is defined and invoked in the GEC, when function x()'s EC is pushed in the call stack, it stores a reference to its parent's lexical environment i.e. the GEC's memory.
+// 1) Lexical Environment of its parent is the scope where a function is physically present
+//    or defined. So, suppose a function x(), is defined and invoked in the GEC,
+//    when function x()'s EC is pushed in the call stack, it stores a reference to its parent's lexical environment i.e. the GEC's memory.
 
-// 2) Whenever a new Execution Context is pushed in the Call Stack it holds a reference to the Lexical Environment of its parent, i.e. the EC's memory from where it was invoked.
+// 2) Whenever a new Execution Context is pushed in the Call Stack it holds a reference
+//    to the Lexical Environment of its parent, i.e. the EC's memory from where it was invoked.
 
 // 3) Global execution context holds reference to null.
 
-// 4)  Javascript engine first looks for the variable/function being accessed in the local scope of the function, and if not found, it keeps on searching the lexical environment of its parent until it finds the variable/function being accessed.
+// 4)  Javascript engine first looks for the variable/function being accessed in the
+//     local scope of the function, and if not found, it keeps on searching the lexical
+//     environment of its parent until it finds the variable/function being accessed.
 
 // 5) The mechanism mentioned in point 4 above is called SCOPE CHAIN.
 
-// 6) If the variable accessed is not found in the Scope Chain, then you will get the variable is not defined error in the  browser's console.
+// 6) If the variable accessed is not found in the Scope Chain,
+//    then you will get the variable is not defined error in the  browser's console.
